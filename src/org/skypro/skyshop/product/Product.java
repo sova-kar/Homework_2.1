ebaf2a7a6eebf5a3f0f7834dfe786c8850301dc9
SOVA <sova16tais@gmail.com>
2025-01-16 00:31:13 +0300

    Did my homework.Part 2

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,8 +1,23 @@
 package org.skypro.skyshop.product;
 
+import java.util.Objects;
+
 public class Product {
     private String name;
     private int price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
 
     public Product(String name, int price){
         this.name = name;
